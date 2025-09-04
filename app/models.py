446f94e140d6890dd9382df033bc953bcc1ffeb6
joaopgs4 <jpgardenalli@gmail.com>
2025-09-04 18:26:55 -0300

    Update: Default models only include user

--- app/models.py
+++ app/models.py
@@ -9,26 +9,12 @@ Base = declarative_base()
 ##### Uses SqlAlchemy bases for static objects; referenced in DB #####
 ######################################################################
 
-#Default password table for saving the user password as a hash256
-#(Receives the hashed string)
-class Password(Base):
-    __tablename__ = 'password'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    password256 = Column(String(256), nullable=False)
-
 #Default generic user table
 #Use for reference on developing new tables
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    uuid = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     username = Column(String(60), nullable=False, unique=True)
     email = Column(String(45), nullable=False, unique=True)
-    id_password = Column(Integer, ForeignKey('password.id'))
-
-    password = relationship("Password")
-
-    __table_args__ = (
-        UniqueConstraint('id_password'),
-    )
+    password256 = Column(String(256), nullable=False)
